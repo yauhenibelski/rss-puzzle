3b345212fc3b7f2b1070da82c61dfc911a37703c
Yauheni Belski <108809026+yauhenibelski@users.noreply.github.com>
2024-03-14 02:39:36 +0100

    feat: implement validation input fields (#13)

--- src/app/pages/login/login-page.ts
+++ src/app/pages/login/login-page.ts
@@ -17,7 +17,8 @@ class LoginPage extends Component {
 
         const firstNameField = <HTMLInputElement>this.elements.firstNameField.firstChild;
         const secondNameField = <HTMLInputElement>this.elements.secondNameField.firstChild;
-        const { loginBTN } = this.elements;
+        const loginBTN = <HTMLInputElement>this.elements.loginBTN;
+        const form = <HTMLFormElement>this.elements.form;
 
         firstNameField.required = true;
         firstNameField.placeholder = 'example: John';
@@ -25,13 +26,25 @@ class LoginPage extends Component {
         secondNameField.required = true;
         secondNameField.placeholder = 'example: Smith';
 
+        firstNameField.oninput = () => firstNameField.setCustomValidity('');
+        secondNameField.oninput = () => secondNameField.setCustomValidity('');
+
         loginBTN.onclick = () => {
-            console.log('login');
+            const validFirstNameField = this.matchInputValue(firstNameField, 2);
+            const validSecondNameField = this.matchInputValue(secondNameField, 3);
+            const canSubmit = validFirstNameField && validSecondNameField;
+
+            firstNameField.className = validFirstNameField ? '' : style.invalid;
+            secondNameField.className = validSecondNameField ? '' : style.invalid;
+
+            console.log(canSubmit);
         };
+        form.onsubmit = (event: Event) => event.preventDefault();
     }
 
     childrenElements(): { [key: string]: HTMLElement } {
         return {
+            form: createElement({ tag: 'form' }),
             firstNameField: createElement({ tag: 'input', style: style['first-name'] }, true),
             secondNameField: createElement({ tag: 'input', style: style['second-name'] }, true),
             loginBTN: createElement({ tag: 'button', text: 'Login' }),
@@ -39,7 +52,34 @@ class LoginPage extends Component {
     }
 
     appendElements(): void {
-        this.contentWrap.append(...Object.values(this.elements));
+        const { form, firstNameField, secondNameField, loginBTN } = this.elements;
+
+        form.append(firstNameField, secondNameField, loginBTN);
+        this.contentWrap.append(form);
+    }
+
+    matchInputValue(input: HTMLInputElement, minLength: number): boolean {
+        const { value, validity } = input;
+        if (value) {
+            if (!value[0].match('[A-Z]')) {
+                input.setCustomValidity('First letter should be in uppercase.');
+                return false;
+            }
+
+            // eslint-disable-next-line prettier/prettier, no-useless-escape
+            if (!value.match('^[a-zA-Z-]+$')) {
+                input.setCustomValidity('Only the symbol "-" is allowed');
+                return false;
+            }
+
+            // eslint-disable-next-line no-useless-escape
+            if (!value.match(`^[a-zA-Z\-]{${minLength}}`)) {
+                input.setCustomValidity(`Minimum length ${minLength + 1} characters`);
+                return false;
+            }
+        }
+        input.setCustomValidity('');
+        return validity.valid;
     }
 }
 
